58e410a9c2b5a5628311e46fee8898bba911ed02
ryan-907 <ryan.thornton@lander.edu>
2023-11-20 14:34:38 -0500

    Improved functionality and added comments

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import pyinputplus as pyin
 from Kinematics import *
-if __name__ == '__main__':
-    print("Welcome to my physics calculator please choose one of the following:")
-    print("1-Calculate force")
-    choice = int(input())
+from material import * 
 
-    if choice == 1:
-        mass = pyin.inputFloat("Pleas enter mass (if unknown, enter 0)")
-        acceleration = pyin.inputFloat("Pleas enter acceleration (if unknown, enter 0)")
-        force = pyin.inputFloat("Pleas enter force (if unknown, enter 0)")
-        print(Force(force, mass, acceleration)) 
+#This ensures anything is either a float or a None. 
+def to_float(x):
+    try:
+        x = float(x)
+        return x
+    except ValueError:
+        return ''
+    
+if __name__ == '__main__':
+    #Greeting :)
+    print("Welcome to my physics calculator please choose one of the following:")
+    print("First lets make an item to experience physics. Leave blank any unknown variables. ")
+    #Taking details about the item. I usually name my item Tim :)
+    n = input("Objects name is: ")
+    f = to_float(input("Force: "))
+    m = to_float(input("Mass: "))
+    a = to_float(input("Acceleration: "))
+    w = to_float(input("Weight: "))
+    d = to_float(input("Distance: "))
+    t = to_float(input("Time: "))
+
+    #Initializing the Data Class
+    item = material(name=n, force=f, mass = m,acceleration=a, weight=w, distance=d, time=t)
+    #Only force right now but I will do others, just you wait!
+    print(f'{item.name} has a force of {Force(item.force, item.mass, item.acceleration)}')
